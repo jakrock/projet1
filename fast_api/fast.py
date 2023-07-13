754308fa48192c9d6e7103b166ffc2b2c5368915
unknown <romualdjja@gmail.com>
2023-07-13 14:50:35 +0200

    mon tout premier

--- fast_api/fast.py
+++ fast_api/fast.py
@@ -119,7 +119,7 @@ async def read_root4():
 
 
 
-@app.get("pre_match/value")
+@app.get("/pre_match/value")
 async def valueBet_preMatch():
     db=client["finale_pre"]
     collection7=db['valuebet']
